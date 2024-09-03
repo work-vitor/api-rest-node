b0770aef2cfae6a679af504e80f7fb9b3f256252
Vitor Rodrigues <Vitor@email.com>
2024-09-03 00:42:05 -0300

    feat: configurando regras

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import tseslint from "@typescript-eslint/eslint-plugin";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-empty-interface': 'off', // Regra correta para desativar interfaces vazias
+    },
+  },
 ];
